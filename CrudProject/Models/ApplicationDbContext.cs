bdc344c25997054ad6f27ae5313b76e88951a734
Lalit <nankarlait7@gmail.com>
2024-12-08 21:37:06 +0530

    Add project files.

---
+++ CrudProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace CrudProject.Models
+{
+    public class ApplicationDbContext : DbContext
+    
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
